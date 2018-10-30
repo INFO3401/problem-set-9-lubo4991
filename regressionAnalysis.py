0053e0125439f742aa22bb7f1bcce41480f34532
lubo4991 <lubo4991@colorado.edu>
2018-10-30 14:14:43 -0600

    Update regressionAnalysis.py

--- regressionAnalysis.py
+++ regressionAnalysis.py
@@ -5,15 +5,19 @@
 import pandas as pd
 import csv
 import parsers
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt  
+#%matplotlib inline
 
 
+# PROBLEM 1. Using the candy-data.csv file in the repo, populate an AnalysisData object that will hold the data you'll use for today's problem set. You should read in the data from the CSV, store the data in the dataset variable, and initialize the xs (column name) and targetY variables appropriately. targetY should reference the variable describing whether or not a candy is chocolate.
 
-#Problem 1
 def _init_(self, type):
     self.type=type
     self.data=[]
     
-    file = open(in_file, encoding = 'utf-8')
+    file = open(filename, encoding = 'utf-8')
     
 def parseFile(self, filename):
     if (self.type =="csv"):
@@ -23,6 +27,43 @@ def parseFile(self, filename):
         else:
             self.data=open('candy-data.csv').read()
 
+#PROBLEM 2. Create a function to initialize a LinearAnalysis object that takes a targetY as its input parameter. Create the same function for LogisticAnalysis. Note that you will use the LinearAnalysis object to try to predict the amount of sugar in the candy and the LogisticAnalysis object to predict whether or not the candy is chocolate.
+
+#dataset.plot(x='competitorname', y='sugarpercent', style='o')  
+#plt.title('candy name vs. sugarpercent')  
+#plt.xlabel('candy name')  
+#plt.ylabel('sugarpercent')  
+#plt.show()  
+
+
+def linearAnalysis_Object(filename, targetY):
+     df = pd.read_csv(filename, encoding='latin1')
+    #prepping the data
+     X = df[[:,1:9]]
+     Y = df[[:, 10]]    #is this where we would fit target Y and target X?
+        
+     #X_features = df.iloc[:,1:9]
+     #Y_features = df.iloc[:, 10]
+    #Split data into training and test sets
+     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)  
+    #Training the algorithm
+     regressor = LinearRegression()  
+     regressor.fit(X_train, y_train) 
+    #intercept
+     #print(regressor.intercept_) 
+    #Slope
+     #print(regressor.coef_) 
+    
+    #Making the prediction
+     y_pred = regressor.predict(X_test) 
+    #Compare test vs. train
+     df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})  
+     df 
+    
+    
+
+    
+#Problem 3
 
 
             
